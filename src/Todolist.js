8f606a30c62e52a21efc519cef5397b09ae138ea
ijimaso <soxup0708@gmail.com>
2019-02-26 21:59:20 +0900

    change a method name

--- src/Todolist.js
+++ src/Todolist.js
@@ -7,18 +7,18 @@ import "./css/Todolist.css";
 //親から渡されたデータ(配列や変数など)をpropsで受け取って使う
 export default class Todolist extends Component {
     // 一行目の個数を指定して，Todoを取り出すメソッド
-    makeLine(lineNumber) {
+    arrangeTodos(lineNumber) {
         // filterメソッドで条件を満たすTodosを取り出す
-        const onelineTodos = this.props.todos.filter(onelineTodo => {
+        const onelineTodos = this.props.todos.filter(todo => {
             return (
-                onelineTodo.id >= 3 * lineNumber - 2 && onelineTodo.id <= lineNumber * 3
+                todo.id >= 3 * lineNumber - 2 && todo.id <= lineNumber * 3
             );
         });
 
         //取り出したTodos(配列)を使って，それぞれのTodoに対して，Todoコンポーネントを呼び出す
         //{...eactTodo}で下の階層にプロパティをまとめて渡せる
-        const eachTodos = onelineTodos.map(eachTodo => {
-            return <Todo key={eachTodo.id} {...eachTodo} />;
+        const eachTodos = onelineTodos.map(onelineTodo => {
+            return <Todo key={onelineTodo.id} {...onelineTodo} />;
         });
 
         //作り出されたTodoコンポーネントを戻り値として返す
@@ -29,10 +29,9 @@ export default class Todolist extends Component {
     render() {
         return (
             <div className="Todolist">
-                <ul>{this.makeLine(1)}</ul>
-                <ul>{this.makeLine(2)}</ul>
-                <ul>{this.makeLine(3)}</ul>
-                <ul>{this.makeLine(4)}</ul>
+                <ul>{this.arrangeTodos(1)}</ul>
+                <ul>{this.arrangeTodos(2)}</ul>
+                <ul>{this.arrangeTodos(3)}</ul>
             </div>
         );
     }
