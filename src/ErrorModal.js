30d8678431842c0550d3b95ee0ee6e5b9a4056f2
ijimaso <soxup0708@gmail.com>
2019-03-27 11:30:30 +0900

    add ErrorModal Component

---
+++ src/ErrorModal.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+
+
+/**
+ * ErrorModalコンポーネント
+ *
+ * @export
+ * @class ErrorModal
+ * @extends {Component}
+ */
+const ErrorModal = props => {
+    return (<Modal show={props.show} onHide={props.modalClose}>
+        <Modal.Header closeButton>
+            <Modal.Title>Error</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{`Please enter a new Todo's ${props.modalMessage}!!`}</Modal.Body>
+        <Modal.Footer>
+            <Button variant="primary" onClick={props.modalClose}>
+                OK!
+                </Button>
+        </Modal.Footer>
+    </Modal>)
+}
+
+export default ErrorModal;
